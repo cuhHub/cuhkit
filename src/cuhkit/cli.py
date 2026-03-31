cf0e87b3198aa26e9f0c15ee51f74073ecc8e806
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2026-03-31 01:31:16 +0100

    feat(cli): add cli context, add functionality to some commands, etc

--- src/cuhkit/cli.py
+++ src/cuhkit/cli.py
@@ -21,35 +21,86 @@ limitations under the License.
 
 # // Imports
 import click
+from pathlib import Path
+from dataclasses import dataclass
+from functools import wraps
+from typing import Callable
 
-from cuhkit import (
-    __VERSION__
-)
-
-from cuhkit.projects import (
-    ProjectType,
-    does_project_exist_at_path,
-    create_addon_project,
-    create_mod_project
-)
-
-from cuhkit.log import set_logging_verbose
+from cuhkit import __VERSION__
+from cuhkit import projects
+from cuhkit.libs.timeit import TimeIt
+from cuhkit.log import set_logging_verbose, logger
 
 # // Main
+def requires_project(project_type: projects.ProjectType | None = None):
+    """
+    Decorator for click commands that require a cuhkit project.
+    
+    Args:
+        project_type (projects.ProjectType | None): The type of project required, or None to allow any.
+    """
+
+    def decorator(function: Callable):
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            context = click.get_current_context()
+            cli_context = get_context(context)
+
+            if cli_context.project is None:
+                logger.error("No cuhkit project found in current directory, this command must be run in a cuhkit project directory.")
+                return
+            
+            if project_type is not None and cli_context.project.project_type != project_type:
+                logger.error(f"Current cuhkit project is not of required type {project_type}, got {cli_context.project.project_type}.")
+                return
+
+            return function(*args, **kwargs, project = cli_context.project)
+        
+        return wrapper
+    
+    return decorator
+
+@dataclass
+class CLIContext:
+    """
+    Context object for the cuhkit CLI.
+    """
+
+    project: projects.AddonProject | projects.ModProject | None
+    
+def get_context(context: click.Context) -> CLIContext:
+    """
+    Gets the CLI context from a click context.
+    
+    Args:
+        context (click.Context): The click context to get the CLI context from.
+        
+    Returns:
+        CLIContext: The CLI context.
+    """
+
+    return context.obj["context"]
 
 @click.group()
 @click.version_option(__VERSION__)
 @click.help_option()
 @click.option("verbose", "--verbose", "-v", is_flag = True, help = "Enables verbose output.")
-def cli(verbose: bool):
+@click.pass_context
+def cli(context: click.Context, verbose: bool):
     """
     Main CLI entry point.
     """
+    
+    context.ensure_object(dict)
+
+    context.obj["context"] = CLIContext(
+        project = projects.load_project_at_path(Path.cwd()) if projects.does_project_exist_at_path(Path.cwd()) else None
+    )
 
     if verbose:
         set_logging_verbose(verbose)
 
-    click.echo("cuhkit - A CLI-oriented Python package for handling cuhHub Stormworks projects (addons/mods).")
+    logger.info("cuhkit - A CLI-oriented Python package for handling cuhHub Stormworks projects (addons/mods).")
 
 @cli.command()
 @click.argument(
@@ -59,49 +110,62 @@ def cli(verbose: bool):
 )
 @click.option(
     "--path", "-p",
-    type = click.Path(exists = True, file_okay = False, dir_okay = True, resolve_path = True),
+    type = click.Path(exists = True, file_okay = False, dir_okay = True, resolve_path = True, path_type = Path),
     default = ".",
     help = "The directory to create the project in."
 )
 @click.option(
     "--type", "-t", "project_type",
-    type = click.Choice(ProjectType),
+    type = click.Choice(projects.ProjectType),
     required = True,
     help = "The type of the project to create."
 )
-def new(name: str, path: str, project_type: ProjectType):
+def new(name: str, path: Path, project_type: projects.ProjectType):
     """
     Create a new cuhkit project.
     """
     
-    if does_project_exist_at_path(path):
-        click.echo(f"A cuhkit project already exists at {path}.", err = True)
+    if projects.does_project_exist_at_path(path):
+        logger.error(f"A cuhkit project already exists at {path}.")
         return
 
-    if project_type == ProjectType.ADDON:
-        create_addon_project(name, path)
-    elif project_type == ProjectType.MOD:
-        create_mod_project(name, path)
+    if project_type == projects.ProjectType.ADDON:
+        project = projects.create_addon_project(name, path)
+    elif project_type == projects.ProjectType.MOD:
+        project = projects.create_mod_project(name, path)
         
-    click.echo(f"Created cuhkit project at {path}.")
+    project.save()
+
+    logger.info(f"Created cuhkit project at {path}.")
 
 @cli.command()
-def clean():
+@click.pass_context
+@requires_project()
+@click.confirmation_option(prompt = "Are you sure you want to delete this cuhkit project?")
+def delete(context: click.Context, project: projects.Project):
     """
-    Cleans a cuhkit project.
+    Deletes a cuhkit project.
     """
 
-    pass
+    project.delete()
+    logger.info("Deleted cuhkit project.")
 
 @cli.command()
-def build():
+@click.pass_context
+@requires_project(projects.ProjectType.ADDON)
+def build(context: click.Context, project: projects.AddonProject):
     """
     Build a cuhkit project.
     """
 
-    pass
+    with TimeIt():
+        project.build()
+
+    logger.info("Built cuhkit project.")
 
 @cli.command()
+@click.pass_context
+@requires_project()
 def publish():
     """
     Publish a cuhkit project to cuhHub.
